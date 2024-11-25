cbcd2e90714e50d53a0b2289ad8c26ad705cc116
zjb <zjb@123.com>
2024-11-25 18:49:32 +0800

    feat: code add

---
+++ src/face/js/event-loop.js
@@ -0,0 +1,20 @@
+async function a1() {
+  console.log(1)
+  await a2()
+  console.log(2)
+}
+async function a2() {
+  console.log(3)
+}
+setTimeout(() => {
+  console.log(4)
+}, 0)
+console.log(5)
+a1()
+new Promise((resolve) => {
+  console.log(6)
+  resolve()
+}).then(() => {
+  console.log(7)
+})
+console.log(8)
