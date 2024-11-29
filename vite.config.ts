240b837ecccdd65e2a7ac0dbfb1f41f204d76fc6
zjb <zjb@123.com>
2024-11-29 13:33:27 +0800

    fix: 路径修复测试22

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -11,59 +11,53 @@ import UnoCSS from 'unocss/vite'
 
 const pathSrc = path.resolve(__dirname, 'src')
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': pathSrc
-    }
-  },
-  plugins: [
-    vue(),
-    UnoCSS(),
-
-    AutoImport({
-      // api
-      imports: ['vue', 'vue-router', 'pinia'],
-      resolvers: [ElementPlusResolver()],
-      eslintrc: { enabled: true } // 给eslint生产的配置 只需要一次,
-    }),
-    Components({
-      resolvers: [
-        // 自动导入 Element Plus 组件
-        ElementPlusResolver()
-      ],
-      // 所有的组件可以自动加载
-      dirs: [
-        'src/components',
-        'src/layout/components',
-        'src/views/**/components'
-      ]
-    }),
-
-    ElementPlus({})
-  ],
-
-  // base: '/vue3-admin', // 这里需要设置为你的仓库名称
-  // base: './', // 替换为你的仓库名
-  server: {
-    // port: 3000,
-    proxy: {
-      '/dev-api': {
-        target: 'http://localhost:3000', // 代理到 NestJS 服务器
-        changeOrigin: true, // 是否修改请求的源头
-        rewrite: (path) => path.replace(/^\/dev-api/, '') // 可选：如果需要删除前缀
-        // rewrite: (path) => path.replace(/^\/dev-api/, '/api') // 可选：如果需要删除前缀
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    resolve: {
+      alias: {
+        '@': pathSrc
       }
-    }
+    },
+    plugins: [
+      vue(),
+      UnoCSS(),
+
+      AutoImport({
+        // api
+        imports: ['vue', 'vue-router', 'pinia'],
+        resolvers: [ElementPlusResolver()],
+        eslintrc: { enabled: true } // 给eslint生产的配置 只需要一次,
+      }),
+      Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver()
+        ],
+        // 所有的组件可以自动加载
+        dirs: [
+          'src/components',
+          'src/layout/components',
+          'src/views/**/components'
+        ]
+      }),
+
+      ElementPlus({})
+    ],
+    base: env.VITE_BASE || '/'
+
+    // base: '/vue3-admin', // 这里需要设置为你的仓库名称
+    // base: './', // 替换为你的仓库名
+    // server: {
+    //   // port: 3000,
+    //   proxy: {
+    //     '/dev-api': {
+    //       target: 'http://localhost:3000', // 代理到 NestJS 服务器
+    //       changeOrigin: true, // 是否修改请求的源头
+    //       rewrite: (path) => path.replace(/^\/dev-api/, '') // 可选：如果需要删除前缀
+    //       // rewrite: (path) => path.replace(/^\/dev-api/, '/api') // 可选：如果需要删除前缀
+    //     }
+    //   }
+    // }
   }
-  // server: {
-  //   port: 3000,
-  //   proxy: {
-  //     '/dev-api': {
-  //       target: 'http://localhost:3000',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/dev-api/, '/api')
-  //     }
-  //   }
-  // }
 })
