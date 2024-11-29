663faf54e41b709b7b8532ee08131decc96da6d9
zjb <zjb@123.com>
2024-11-29 13:41:36 +0800

    feat: docs update

--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
   ],
   // base: env.VITE_BASE || '/'
 
-  base: '/vue3-admin/' // 这里需要设置为你的仓库名称
+  base: '/vue3-admin' // 这里需要设置为你的仓库名称
   // base: './', // 替换为你的仓库名
   // server: {
   //   // port: 3000,
