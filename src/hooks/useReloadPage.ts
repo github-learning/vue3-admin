70c805f0fb7121ad1b3e8becf35c1698a673aa32
zhaojiangbing <1846027296@qq.com>
2024-12-13 00:28:50 +0800

    feat: menu

--- src/hooks/useReloadPage.ts
+++ src/hooks/useReloadPage.ts
@@ -1,13 +1,12 @@
-export const useReloadPage = ()=>{
-  const {proxy} = getCurrentInstance()!
-  const reloadPage = async ({title= '是否刷新', message= '确定'}={})=>{
+export const useReloadPage = () => {
+  const { proxy } = getCurrentInstance()!
+  const reloadPage = async ({ title = '是否刷新', message = '确定' } = {}) => {
     try {
-           await proxy.$confirm(title, message)
-    window.location.reload()
-    } catch (error) {
-       proxy?.$message.warning("已经取消了刷新")
+      await proxy?.$confirm(title, message)
+      window.location.reload()
+    } catch {
+      proxy?.$message.warning('已经取消了刷新')
     }
   }
   return reloadPage
-  
 }
